c8ec172ed403f26f4f42e46fc4716d50cdb7e1af
tahsinraza95 <tahsinr693@gmail.com>
2023-02-26 22:15:07 +0530

    Added: Verifying Name

---
+++ User Registration By Lambda/User Registration By Lambda/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace User_Registration_By_Lambda
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine(" welcome User_Registration Program");
+
+            Fillinfo fillinfo = new Fillinfo();
+            fillinfo.fill();
+
+        }
+    }
+
+}
