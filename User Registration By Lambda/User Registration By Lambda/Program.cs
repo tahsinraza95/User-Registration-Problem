fd0eeeaf3010fee3a661b34616f59ca52b41389f
tahsinraza95 <tahsinr693@gmail.com>
2023-02-27 02:26:43 +0530

    Added: Verifying Email

--- User Registration By Lambda/User Registration By Lambda/Program.cs
+++ User Registration By Lambda/User Registration By Lambda/Program.cs
@@ -1,18 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
-namespace User_Registration_By_Lambda
+namespace User_RegistrationByLambda
 {
-    public class Program
+    public class Pattern
     {
-        public static void Main(string[] args)
-        {
+        public static string FirstName = @"^[A-Z][a-z]{2,}$";
+        public static string LastName = @"^[A-Z][a-z]{2,}$";
+        public static string Email = @"^(?=[a-z])[a-z 0-9.!#+$%&_-]{3,}[@][a-z 0-9]{1,6}.(com|com.com|io|com.au|net|org)$";
 
-            Console.WriteLine(" welcome User_Registration Program");
 
-            Fillinfo fillinfo = new Fillinfo();
-            fillinfo.fill();
-
-        }
     }
+    public class RegistrationForm
+    {
+        public bool Firstname(string userName)
+        {
+            Regex name = new Regex(Pattern.FirstName);
+            return name.IsMatch(userName);
+        }
 
+        public bool LastName(string userLastName)
+        {
+            Regex lastname = new Regex(Pattern.LastName);
+            return lastname.IsMatch(userLastName);
+        }
+        public bool Email(string userEmail)
+        {
+            Regex email = new Regex(Pattern.Email);
+            return email.IsMatch(userEmail);
+        }
+
+
+    }
 }
