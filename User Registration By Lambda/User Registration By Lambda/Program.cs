d2d3b9b3b3fbba7054d54cee04751032bf07c5ef
tahsinraza95 <tahsinr693@gmail.com>
2023-02-26 22:47:49 +0530

    Added: Edit Verifying Name

--- User Registration By Lambda/User Registration By Lambda/Program.cs
+++ User Registration By Lambda/User Registration By Lambda/Program.cs
@@ -1,18 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
-namespace User_Registration_By_Lambda
+namespace User_RegistrationByLambda
 {
-    public class Program
+    public class Pattern
     {
-        public static void Main(string[] args)
+        public static string FirstName = @"^[A-Z]{1}[a-z]{2,}$";
+
+    }
+    public class RegistrationForm
+    {
+        public bool Firstname(string userName)
         {
-
-            Console.WriteLine(" welcome User_Registration Program");
-
-            Fillinfo fillinfo = new Fillinfo();
-            fillinfo.fill();
-
+            Regex name = new Regex(Pattern.FirstName);
+            return name.IsMatch(userName);
         }
     }
-
 }
